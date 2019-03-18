0840255184e32de459ee0c90b09279c2f4d68601
Neno0o <a.gamal.saddek@gmail.com>
2019-03-18 11:02:47 +0100

    Update samples

--- samplejava/src/main/java/com/example/webtrekk/samplejava/MainActivity.java
+++ samplejava/src/main/java/com/example/webtrekk/samplejava/MainActivity.java
@@ -1,3 +1,28 @@
+/*
+ *  MIT License
+ *
+ *  Copyright (c) 2019 Webtrekk GmbH
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ *
+ */
+
 package com.example.webtrekk.samplejava;
 
 import android.os.Bundle;
@@ -7,10 +32,10 @@ import java.util.Map;
 
 import androidx.appcompat.app.AppCompatActivity;
 import webtrekk.android.sdk.Webtrekk;
-import webtrekk.android.sdk.model.Param;
-import webtrekk.android.sdk.model.ParamType;
+import webtrekk.android.sdk.Param;
+import webtrekk.android.sdk.ParamType;
 
-import static webtrekk.android.sdk.model.ParamTypeKt.createCustomParam;
+import static webtrekk.android.sdk.ParamTypeKt.createCustomParam;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +51,6 @@ public class MainActivity extends AppCompatActivity {
         params.put(BACKGROUND_PARAM, "lol");
         params.put(Param.INTERNAL_SEARCH, "lel");
 
-        webtrekk.trackPage("my page", params);
+        webtrekk.trackCustomPage("my page", params);
     }
 }
