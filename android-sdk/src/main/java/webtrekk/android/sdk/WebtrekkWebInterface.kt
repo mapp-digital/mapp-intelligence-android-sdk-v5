1a36aab0d5f373a92f769fd52db2868192dbef6d
aleksandar.marinkovi <aleksandar.marinkovic@mapp.com>
2019-08-20 16:12:48 +0200

    Code refactoring

--- android-sdk/src/main/java/webtrekk/android/sdk/WebtrekkWebInterface.kt
+++ android-sdk/src/main/java/webtrekk/android/sdk/WebtrekkWebInterface.kt
@@ -66,7 +66,7 @@ open class WebtrekkWebInterface(private val webtrekk: Webtrekk) {
         try {
             webtrekk.trackCustomPage(pageName, params.jsonToMap())
         } catch (e: Exception) {
-            webtrekkLogger.info(e.message ?: "Something is a wrong")
+            webtrekkLogger.info(e.message ?: "Unknown exception caught in WebView while tracking custom page")
         }
     }
 
@@ -78,7 +78,7 @@ open class WebtrekkWebInterface(private val webtrekk: Webtrekk) {
         try {
             webtrekk.trackCustomEvent(eventName, params.jsonToMap())
         } catch (e: Exception) {
-            webtrekkLogger.info(e.message ?: "Something is a wrong")
+            webtrekkLogger.info(e.message ?: "Unknown exception caught in WebView while tracking custom page")
         }
     }
 
