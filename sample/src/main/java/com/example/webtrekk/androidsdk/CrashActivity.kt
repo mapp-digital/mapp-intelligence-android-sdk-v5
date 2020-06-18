e9a94a4babe544d1c5040ebe684dfad838bb6c4c
Ivan Stevanovic <ivan.stevanovic@mapp.com>
2020-06-18 17:44:17 +0200

    fixed writing and reading exception from file

--- sample/src/main/java/com/example/webtrekk/androidsdk/CrashActivity.kt
+++ sample/src/main/java/com/example/webtrekk/androidsdk/CrashActivity.kt
@@ -3,18 +3,17 @@ package com.example.webtrekk.androidsdk
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import webtrekk.android.sdk.Webtrekk
-import java.lang.NumberFormatException
 
 class CrashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crash)
-
+        Integer.parseInt("@!#")
         try {
             Integer.parseInt("@!#")
-        } catch (numberFormatException: NumberFormatException) {
-            Webtrekk.getInstance().trackException(numberFormatException)
+        } catch (e: Exception) {
+            Webtrekk.getInstance().trackException(e)
             Webtrekk.getInstance().trackException("Ivan", "Momak")
         }
     }
