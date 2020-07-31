e69d4f06d6e46d48077aff03ce95907a477eeb94
aleksandar.marinkovi <aleksandar.marinkovic@mapp.com>
2020-07-31 13:48:55 +0200

    unit test quick fix

---
+++ android-sdk/src/main/java/webtrekk/android/sdk/integration/IntelligenceEvent.kt
@@ -0,0 +1,24 @@
+package webtrekk.android.sdk.integration
+
+import android.content.Context
+import android.content.Intent
+
+/**
+ * Created by Aleksandar Marinkovic on 30/07/2020.
+ * Copyright (c) 2020 MAPP.
+ */
+internal object IntelligenceEvent {
+    fun sendEvent(
+        context: Context,
+        event: String?,
+        data: String?
+    ) {
+        val intent: Intent = Intent(MappIntelligenceListener.WebtrekkToMapp).setClassName(
+            context,
+            "webtrekk.android.sdk.integration.MappIntelligenceListener"
+        )
+        intent.putExtra(MappIntelligenceListener.INTELLIGENCE_DATA, data)
+        intent.type = event
+        context.sendBroadcast(intent)
+    }
+}
