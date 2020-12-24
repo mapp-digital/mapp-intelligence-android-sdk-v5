e94abad156e342153e9a42a4cca68b909121a77e
aleksandar.marinkovi <aleksandar.marinkovic@mapp.com>
2020-12-24 16:46:08 +0100

    Test application for deep linking tracking

---
+++ sample/src/main/java/com/example/webtrekk/androidsdk/mapp/MyPushBroadcastReceiver.kt
@@ -0,0 +1,33 @@
+package com.example.webtrekk.androidsdk.mapp
+
+import android.util.Log
+import com.appoxee.push.PushData
+import com.appoxee.push.PushDataReceiver
+
+/**
+ * Created by alexeykrichun on 11/07/16.
+ */
+class MyPushBroadcastReceiver : PushDataReceiver() {
+    override fun onPushReceived(pushData: PushData) {
+        Log.d("Engage", "Push received $pushData")
+        super.onPushReceived(pushData)
+    }
+
+    override fun onPushOpened(pushData: PushData) {
+        Log.d("APX", "Push opened $pushData")
+        //  super.onPushReceived(pushData);
+    }
+
+    override fun onPushDismissed(pushData: PushData) {
+        Log.d("APX", "Push dismissed $pushData")
+        //   super.onPushReceived(pushData);
+    }
+
+    override fun onSilentPush(pushData: PushData) {
+        Log.d("APX", "Push Silent $pushData")
+    }
+
+    override fun onButtonClick(pushData: PushData, buttonAction: String, buttonPosition: Int) {
+        Log.d("APX", "Button clicked: $pushData")
+    }
+}
