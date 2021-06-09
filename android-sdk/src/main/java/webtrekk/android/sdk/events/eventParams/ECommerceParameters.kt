a51732b712f4720bc76458c51a17e102ceb769e1
aleksandar.marinkovi <aleksandar.marinkovic@mapp.com>
2021-06-09 10:57:46 +0200

    new param change

--- android-sdk/src/main/java/webtrekk/android/sdk/events/eventParams/ECommerceParameters.kt
+++ android-sdk/src/main/java/webtrekk/android/sdk/events/eventParams/ECommerceParameters.kt
@@ -38,15 +38,12 @@ constructor(
     var returnValue: Number? = null
     var cancellationValue: Number? = null
     var couponValue: Number? = null
-    var productAdvertiseID: Number? = null
-    var productSoldOut: Number? = null
     var paymentMethod: String? = null
     var shippingServiceProvider: String? = null
     var shippingSpeed: String? = null
     var shippingCost: Number? = null
     var markUp: Number? = null
     var orderStatus: String? = null
-    var productVariant: String? = null
     override fun toHasMap(): MutableMap<String, String> {
         val map = mutableMapOf<String, String>()
         if (!customParameters.isNullOrEmpty()) {
@@ -62,29 +59,83 @@ constructor(
         map.addNotNull(ECommerceParam.RETURN_VALUE, returnValue)
         map.addNotNull(ECommerceParam.CANCELLATION_VALUE, cancellationValue)
         map.addNotNull(ECommerceParam.COUPON_VALUE, couponValue)
-        map.addNotNull(ECommerceParam.PRODUCT_ADVERTISE_ID, productAdvertiseID)
-        map.addNotNull(ECommerceParam.PRODUCT_SOLD_OUT, productSoldOut)
+
         map.addNotNull(ECommerceParam.PAYMENT_METHOD, paymentMethod)
         map.addNotNull(ECommerceParam.SHIPPING_SERVICE_PROVIDER, shippingServiceProvider)
         map.addNotNull(ECommerceParam.SHIPPING_SPEED, shippingSpeed)
         map.addNotNull(ECommerceParam.SHIPPING_COST, shippingCost)
         map.addNotNull(ECommerceParam.MARK_UP, markUp)
         map.addNotNull(ECommerceParam.ORDER_STATUS, orderStatus)
-        map.addNotNull(ECommerceParam.PRODUCT_VARIANT, productVariant)
+
         var productNames = mutableListOf<String>()
         var categoriesKeys = mutableListOf<Int>()
+        var productEcommerceParametersKeys = mutableListOf<Int>()
         var productCosts = mutableListOf<String>()
         var productQuantities = mutableListOf<String>()
+        var productAdvertiseID = mutableListOf<String>()
+        var productSoldOut = mutableListOf<String>()
+        var productVariant = mutableListOf<String>()
+        var productCostsEmpty = true
+        var productQuantitiesEmpty = true
+        var productAdvertiseIDEmpty = true
+        var productSoldOutEmpty = true
+        var productVariantEmpty = true
         if (products.isNotEmpty()) {
             products.forEach { product ->
                 productNames.add(product.name)
-                productCosts.add(if (product.cost != null) product.cost.toString() else "")
-                productQuantities.add(if (product.quantity != null) product.quantity.toString() else "")
+                productCosts.add(
+                    if (product.cost != null) {
+                        productCostsEmpty = false
+                        product.cost.toString()
+                    } else ""
+                )
+                productQuantities.add(
+                    if (product.quantity != null) {
+                        productQuantitiesEmpty = false
+                        product.quantity.toString()
+                    } else ""
+                )
                 product.categories.forEach { (key, _) ->
                     if (!categoriesKeys.contains(key))
                         categoriesKeys.add(key)
                 }
+
+                product.ecommerceParameters.forEach { (key, _) ->
+                    if (!productEcommerceParametersKeys.contains(key))
+                        productEcommerceParametersKeys.add(key)
+                }
+                productAdvertiseID.add(
+                    if (product.productAdvertiseID != null) {
+                        productAdvertiseIDEmpty = false
+                        product.productAdvertiseID.toString()
+                    } else ""
+                )
+                productSoldOut.add(
+                    if (product.productSoldOut != null) {
+                        productSoldOutEmpty = false
+                        product.productSoldOut.toString()
+                    } else ""
+                )
+                productVariant.add(
+                    if (product.productVariant != null) {
+                        productVariantEmpty = false
+                        product.productVariant.toString()
+                    } else ""
+                )
             }
+            productEcommerceParametersKeys.forEach { maine ->
+                val tempCategory = mutableListOf<String>()
+                products.forEach { product ->
+                    if (product.ecommerceParameters.containsKey(maine)) {
+                        tempCategory.add(product.ecommerceParameters[maine]!!)
+                    }
+                }
+                map.addNotNull(
+                    "${ECommerceParam.COMMERCE_PARAM}$maine",
+                    tempCategory.joinToString(";")
+                )
+            }
+
             categoriesKeys.forEach { maine ->
                 val tempCategory = mutableListOf<String>()
                 products.forEach { product ->
@@ -97,11 +148,24 @@ constructor(
                     tempCategory.joinToString(";")
                 )
             }
-
+            if (!productAdvertiseIDEmpty)
+                map.addNotNull(
+                    ECommerceParam.PRODUCT_ADVERTISE_ID,
+                    productAdvertiseID.joinToString(";")
+                )
+            if (!productSoldOutEmpty)
+                map.addNotNull(ECommerceParam.PRODUCT_SOLD_OUT, productSoldOut.joinToString(";"))
+            if (!productVariantEmpty)
+                map.addNotNull(ECommerceParam.PRODUCT_VARIANT, productVariant.joinToString(";"))
             map.addNotNull(ECommerceParam.PRODUCT_NAME, productNames.joinToString(";"))
-            map.addNotNull(ECommerceParam.PRODUCT_COST, productCosts.joinToString(";"))
-            if (status != Status.VIEWED)
-                map.addNotNull(ECommerceParam.PRODUCT_QUANTITY, productQuantities.joinToString(";"))
+            if (!productCostsEmpty)
+                map.addNotNull(ECommerceParam.PRODUCT_COST, productCosts.joinToString(";"))
+            if (!productQuantitiesEmpty)
+                if (status != Status.VIEWED)
+                    map.addNotNull(
+                        ECommerceParam.PRODUCT_QUANTITY,
+                        productQuantities.joinToString(";")
+                    )
         }
         return map
     }
