a78d5fa742d0034ae9bc20d3f7c45c0c7973b442
aleksandar.marinkovi <aleksandar.marinkovic@mapp.com>
2021-07-01 13:46:41 +0200

    Anonymous Tracking test application

--- sample/src/main/java/com/example/webtrekk/androidsdk/SampleApplication.kt
+++ sample/src/main/java/com/example/webtrekk/androidsdk/SampleApplication.kt
@@ -83,6 +83,5 @@ class SampleApplication : Application() {
                 .build()
 
         Webtrekk.getInstance().init(this, webtrekkConfigurations)
-        Webtrekk.getInstance().anonymousTracking(true,setOf("la", "cs804", "cs821"),generateNewEverId = true)
     }
 }
