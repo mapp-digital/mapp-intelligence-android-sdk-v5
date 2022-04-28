c451af956b695f284ffeb60c1568e53cc7bcf6dc
semsudin.tafilovic <sekitafilovic@gmail.com>
2022-04-28 02:30:33 +0200

    Added everId parameter into webtrekkConfiguration; Set everId on initialization method

---
+++ android-sdk/src/test/java/webtrekk/android/sdk/util/DataExtensionClass.kt
@@ -0,0 +1,20 @@
+package webtrekk.android.sdk.util
+
+import org.junit.Assert.assertEquals
+import org.junit.jupiter.api.Test
+
+
+class DataExtensionClass {
+
+    @Test
+    fun testWebtrekkAppVersionSplittin(){
+        val version="5.1.4.1"
+
+        val trimmedVersion=version.split(".")
+            .subList(0,3)
+            .joinToString(separator = "")
+            .substring(0,3)
+
+        assertEquals(trimmedVersion, "514")
+    }
+}
