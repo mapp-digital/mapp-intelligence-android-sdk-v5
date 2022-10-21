ae1a3432027ff45e4432c3de942fc041a0085f7a
semsudin.tafilovic <sekitafilovic@gmail.com>
2022-10-21 17:17:39 +0200

    Fixed migration from old SDK v4 to SDK v5

---
+++ sample-v4/src/main/java/com/example/webtrekk/androidsdk/SampleApplication.kt
@@ -0,0 +1,12 @@
+package com.example.webtrekk.androidsdk
+
+import android.app.Application
+import com.webtrekk.webtrekksdk.Webtrekk
+
+class SampleApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
