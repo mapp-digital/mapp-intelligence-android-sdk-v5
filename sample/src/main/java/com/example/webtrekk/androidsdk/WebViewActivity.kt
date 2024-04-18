56c7335fa3c7dd1f99a69860d08b725dfe9ab343
Semsudin Tafilovic <sekitafilovic@gmail.com>
2024-04-18 10:34:01 +0200

    Updated WebView tracking page - added basic auth

--- sample/src/main/java/com/example/webtrekk/androidsdk/WebViewActivity.kt
+++ sample/src/main/java/com/example/webtrekk/androidsdk/WebViewActivity.kt
@@ -27,6 +27,9 @@ package com.example.webtrekk.androidsdk
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.HttpAuthHandler
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import com.example.webtrekk.androidsdk.databinding.WebViewActivityBinding
 import webtrekk.android.sdk.Webtrekk
@@ -45,7 +48,22 @@ class WebViewActivity : AppCompatActivity() {
             WebtrekkWebInterface(Webtrekk.getInstance()),
             WebtrekkWebInterface.TAG
         )
-        binding.webView.settings.javaScriptEnabled = true
-        binding.webView.loadUrl("http://demoshop.webtrekk.com/web2app/index.html")
+        binding.webView.settings.apply {
+            javaScriptEnabled=true
+            textZoom=100
+        }
+
+        binding.webView.webViewClient=object :WebViewClient(){
+            override fun onReceivedHttpAuthRequest(
+                view: WebView?,
+                handler: HttpAuthHandler?,
+                host: String?,
+                realm: String?
+            ) {
+                handler?.proceed("demo","demo")
+            }
+        }
+        //binding.webView.loadUrl("http://demoshop.webtrekk.com/web2app/index.html")
+        binding.webView.loadUrl("https://www.zeit.de/geld/2023-07/chatgpt-plugins-ki-aktien-etfs-portfolio-pilot")
     }
 }
